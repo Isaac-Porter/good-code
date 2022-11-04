eae945aeba277fb5b3ab971a5477b8f40c0fa837
Bubboreal <116916200+Bubboreal@users.noreply.github.com>
2022-11-04 17:20:48 -0500

    d

---
+++ include/graph-stuff.h
@@ -0,0 +1,7 @@
+#include "vex.h"
+
+//extern double g_input;
+extern double g_speed;
+extern double g_target;
+
+int graph();
---
+++ src/graph-stuff.cpp
@@ -0,0 +1,26 @@
+#include "graph-stuff.h"
+double g_input=0;
+int y,t,py;
+int graph(){
+    t=0;
+    g_input=0;
+    py=0;
+    Brain.Screen.clearScreen();
+    Brain.Screen.setOrigin(0,240);
+    Brain.Screen.setPenColor(green);
+    Brain.Screen.drawLine(0,-168,480,-168);
+    double scale=168.0/g_target;
+    Brain.Screen.setPenColor(white);
+    Brain.Screen.drawPixel(1,-1);
+    while(true){
+        Brain.Screen.printAt(20,-220, "%.3f",g_input);
+        g_input=1+(L1.position(degrees)+L2.position(degrees)+L3.position(degrees)+R1.position(degrees)+R2.position(degrees)+R3.position(degrees))/6;
+        y=-(scale*g_input);
+        Brain.Screen.drawLine(t,y,t-1,py);
+        py=y;
+        t+=1;
+        wait(70,msec);
+    }
+
+    return 1;
+}
